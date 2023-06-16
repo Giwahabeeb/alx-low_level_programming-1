eac11e283b3ea2d580dcb48fd6736b2aa8d67593
Giwahabeeb <habeebgiwa401@gmail.com>
2023-06-16 21:05:37 +0100

    hsdh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,10 @@ int main(void)
  * Return: must return 0
  */
 {
-printf ("Size of a char: %u byte(s)\n", sizeof(char));
-printf ("Size of an int: %d byte(s)\n", sizeof(int));
-printf ("Size of a long int: %ld byte(s)\n", sizeof(long int));
-printf ("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-printf ("Size of a float: %d byte(s)\n", sizeof(float));
-return(0);
-
-
-
-
-
-
+printf("Size of a char: %u byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
 }
