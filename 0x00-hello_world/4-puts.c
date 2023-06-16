2233664d5a769839b3f819b645761ce0e5cdef7c
Giwahabeeb <habeebgiwa401@gmail.com>
2023-06-16 17:41:57 +0100

    hdfshd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include "stdio.h"
-
+/**
+ * main - Programming is like building a multilingual puzzle
+ * * Return:0 (Success the program return)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
