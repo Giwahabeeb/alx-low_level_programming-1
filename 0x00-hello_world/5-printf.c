c9eed7a878b8c788e5caa8c6f18f36cd1cbae030
Giwahabeeb <habeebgiwa401@gmail.com>
2023-06-16 18:55:10 +0100

    hahdh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,6 @@
  * habex
  */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art, \n");
+{printf (""\"with proper grammar, but the outcome is a piece of art,");
 return(0);
 }
