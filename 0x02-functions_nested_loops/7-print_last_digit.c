1f8ca24b55b1758bd5eb9ee926a0c87319ba304e
Giwahabeeb <habeebgiwa401@gmail.com>
2023-06-22 19:02:03 +0100

    jajh'

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+  int last;
+  if (last < 0)
+  {
+    last = last * -1;
+  }
+  _putchar(last + '0');
+  return (last);
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
