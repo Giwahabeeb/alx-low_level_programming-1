b905309150155c877f91f1febb549ac7c47e6909
Giwahabeeb <habeebgiwa401@gmail.com>
2023-06-22 18:32:36 +0100

    jfjj

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,7 @@
  * Return: always 0 (success)
  */
 int main(void)
-{
-_putchar('_');
+{_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -16,5 +15,4 @@ _putchar('h');
 _putchar('a'); 
 _putchar('r');
 _putchar('\n');
-return (0);
-}
+return (0);}
